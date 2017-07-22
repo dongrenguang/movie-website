ec356ca8cd6e7ed546999393ce467309b65b30bd
dongrenguang <dongrenguang@gmail.com>
2017-07-22 19:45:50 +0800

    Complete

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -63,8 +63,7 @@ main_page_head = '''
         });
         // Start playing the video whenever the trailer modal is opened
         $(document).on('click', '.movie-tile', function (event) {
-            var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
-            var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
+            var sourceUrl = $(this).attr('data-source-url') + '?autoplay=1&html5=1';
             $("#trailer-video-container").empty().append($("<iframe></iframe>", {
               'id': 'trailer-video',
               'type': 'text-html',
@@ -99,7 +98,7 @@ main_page_content = '''
         </div>
       </div>
     </div>
-    
+
     <!-- Main Page Content -->
     <div class="container">
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -119,7 +118,7 @@ main_page_content = '''
 
 # A single movie entry html template
 movie_tile_content = '''
-<div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
+<div class="col-md-6 col-lg-4 movie-tile text-center" data-source-url="{source_url}" data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
     <h2>{movie_title}</h2>
 </div>
@@ -129,16 +128,11 @@ def create_movie_tiles_content(movies):
     # The HTML content for this section of the page
     content = ''
     for movie in movies:
-        # Extract the youtube ID from the url
-        youtube_id_match = re.search(r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
-        youtube_id_match = youtube_id_match or re.search(r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
-        trailer_youtube_id = youtube_id_match.group(0) if youtube_id_match else None
-
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
             movie_title=movie.title,
             poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=trailer_youtube_id
+            source_url=movie.trailer_url
         )
     return content
 
--- media.py
+++ media.py
@@ -1,8 +1,7 @@
 import webbrowser
 
 class Movie():
-    """ 电影类，包含电影标题、海报图片链接、youtube上的预告片地址等 """
-    def __init__(self, title, poster_image_url, trailer_youtube_url):
+    def __init__(self, title, poster_image_url, trailer_url):
         self.title = title
         self.poster_image_url = poster_image_url
-        self.trailer_youtube_url  = trailer_youtube_url
+        self.trailer_url  = trailer_url
